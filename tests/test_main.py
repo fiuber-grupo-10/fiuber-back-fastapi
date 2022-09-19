369b83fc4d178790c87bd3aa22d8d9f1cfdfd176
leogm99 <lgiampier62@gmail.com>
2022-09-19 13:22:17 -0300

    CI workflow, pre-commit hooks to sync requirements.txt

---
+++ tests/test_main.py
@@ -0,0 +1,22 @@
+# by @tomyrd (https://github.com/tomyrd/heroku-docker-demo/)
+
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_greeting():
+	response = client.get("/greeting/Tomy")
+	assert response.status_code == 200
+	assert response.json() == "Hola Tomy! Te quiero mucho <3"
+
+
+def test_read_no_vowels():
+	response = client.get("/no_vowels/murcielago")
+	assert response.status_code == 200
+	assert response.json() == "mrclg"
+
+	response = client.get("/no_vowels/hola")
+	assert response.status_code == 200
+	assert response.json() == "hl"
