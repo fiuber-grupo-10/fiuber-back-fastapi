7115c033298a8dfa76f511f5b339226f65917bca
leogm99 <lgiampier62@gmail.com>
2022-09-29 21:08:16 -0300

    Database, controller and models modules, added config object to ease environment configuration
    Also removed old mockup tests and more boilerplate to clean up the repo template

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,25 @@
-# by @tomyrd (https://github.com/tomyrd/heroku-docker-demo/)
-
 from fastapi.testclient import TestClient
-from app.main import app
+from ..app.main import app, get_db
+from ..app.database.db import create_session_from_url
+
+""" Fake DB to test stuff (should add mock) """
+
+
+def override_get_db():
+    db_session = create_session_from_url('postgresql:///./test.db')()
+    try:
+        yield db_session
+    finally:
+        db_session.close()
+    pass
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
 
 def test_read_greeting():
-    response = client.get("/greeting/Tomy")
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == "Hola Tomy! Te quiero mucho <3"
-
-
-""" Tests vowels endpoint """
-
-
-def test_read_no_vowels():
-    response = client.get("/no_vowels/murcielago")
-    assert response.status_code == 200
-    assert response.json() == "mrclg"
-
-    response = client.get("/no_vowels/hola")
-    assert response.status_code == 200
-    assert response.json() == "hl"
+    assert response.json() == "Hello World"
